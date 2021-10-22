a4d105e0957262af0941cb4179ae1adee624e1a7
Pedro Henrique dos Santos Guedes <peguedezz@gmail.com>
2021-10-21 23:18:33 -0300

    Algotitimo Seletion Sort
    Estrutura inicial do algoritmo

--- algoritimos/SelectionSort.java
+++ algoritimos/SelectionSort.java
@@ -10,7 +10,36 @@ package algoritimos;
  * @author Guedes
  */
 public class SelectionSort{
+public class SelectionSort {
     public static void main(String[] args) {
         
+        //fazendo o vetor com 100 posições
+        int[] vetor = new int[10]; 
+        for(int i = 0; i < vetor.length;i++){
+            
+            //colocando numeros aleatorios no vetor;
+            //transformando os valores floats em inteiros
+            vetor[i] = (int)(Math.random() * vetor.length); 
+            
+        }
+
+        //Algoritiom de Ordenação
+        int menor,aux;
+        
+        for(int i=0; i < vetor.length; i++){
+            menor = i;
+            for(int j = i+1; j< vetor.length; j++)
+                if (vetor[j] < vetor[menor]){
+                    menor = j;
+            aux = vetor[menor];
+            vetor[menor] = vetor[i];
+            vetor[i] = aux;
+            }
+        }
+        
+        System.out.println("Validação do Algoritimo");
+        for(int i = 0; i < vetor.length;i++){
+            System.out.println(vetor[i]);
+        }
     }
 }
